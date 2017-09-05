24aa29ac9f9985ee81b3ca4afa8b3783589b9a7a
timkeiner <keiner.tim@gmail.com>
2017-09-05 14:40:26 +0200

    solved aot compilation problems

--- sakuli-admin-web/src/main/java/org/sweetest/platform/server/WebConfig.java
+++ sakuli-admin-web/src/main/java/org/sweetest/platform/server/WebConfig.java
@@ -4,13 +4,16 @@ import org.springframework.boot.autoconfigure.web.ErrorViewResolver;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.util.Collections;
 import java.util.Map;
 
@@ -20,11 +23,13 @@ import java.util.Map;
 @Configuration
 public class WebConfig extends WebMvcConfigurerAdapter {
 
-
-
-    private static final int CACHE_PERIOD_ONE_YEAR = 31536000;
-
-    private static final int CACHE_PERIOD_NO_CACHE = 0;
+/*
+    @Bean
+    public ErrorViewResolver supportPathBasedLocationStrategyWithoutHashes() {
+        return (HttpServletRequest req, HttpStatus status, Map<String, Object> model) -> status == HttpStatus.NOT_FOUND
+                ? new ModelAndView("classpath:/index.html", Collections.emptyMap(), HttpStatus.OK)
+                : null;
+    }
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
@@ -33,28 +38,21 @@ public class WebConfig extends WebMvcConfigurerAdapter {
         registry.addResourceHandler("/styles.css").addResourceLocations("/styles.css").setCachePeriod(CACHE_PERIOD_ONE_YEAR);
         registry.addResourceHandler("/app/**").addResourceLocations("/app/").setCachePeriod(CACHE_PERIOD_NO_CACHE);
         registry.addResourceHandler("/").addResourceLocations("/index.html").setCachePeriod(CACHE_PERIOD_NO_CACHE);
+        registry.addResourceHandler("/**").addResourceLocations("/", "classpath:/");
+    }
         */
-    }
 
-    @Bean
-    public ErrorViewResolver supportPathBasedLocationStrategyWithoutHashes() {
-        return (HttpServletRequest req, HttpStatus status, Map<String, Object> model) -> status == HttpStatus.NOT_FOUND
-                ? new ModelAndView("index.html", Collections.emptyMap(), HttpStatus.OK)
-                : null;
-    }
-
-
-    @Override
-    public void addViewControllers(ViewControllerRegistry registry) {
-        //registry.addRedirectViewController("/**", "/index.html");
-    }
-
-    /*
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(getProjectSetupInterceptor());
+        registry.addInterceptor(new Interceptor());
     }
 
-    */
+    class Interceptor extends HandlerInterceptorAdapter {
+        @Override
+        public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
 
+            System.out.println("CTX-P: " + request.getContextPath());
+            return true;
+        }
+    }
 }
