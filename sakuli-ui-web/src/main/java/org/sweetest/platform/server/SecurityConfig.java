390dca9d018c96ef746486e855375e8495cc0cf8
Tim Keiner <keiner.tim@gmail.com>
2018-04-02 15:02:10 +0200

    whitelist all calls to vnc proxy

--- sakuli-ui-web/src/main/java/org/sweetest/platform/server/SecurityConfig.java
+++ sakuli-ui-web/src/main/java/org/sweetest/platform/server/SecurityConfig.java
@@ -65,7 +65,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                         .antMatchers(HttpMethod.GET, "/", "/**/*.js", "/**/*.css", "/**/*.woff*")
                             .permitAll()
                     // TODO: Right now sockets are not secured, this is actually not the best way
-                        .antMatchers("/api/info" , "/api/socket/**")
+                        .antMatchers("/api/info" ,"/api/novnc/**" , "/api/socket/**")
                             .permitAll()
                         .antMatchers("/api/**")
                             .authenticated()
